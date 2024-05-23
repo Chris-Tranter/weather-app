24ba243e3a6fbf023ded1582ab5391b080b2c483
Chris Tranter <135023953+Chris-Tranter@users.noreply.github.com>
2024-05-23 14:28:20 +0100

    first javascript object

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,10 @@
 const apiKey = "a3f92f91eb9c3564f2a410ca17f082ba";
 const units = "metric";
 
-const forecast = 
+let forecast = {
+    bath: "15",
+    bristol: "16",
+    cardiff: "17"
+};
+
+console.log(forecast.bristol);
