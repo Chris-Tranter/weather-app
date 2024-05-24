eef73e68c7101458f30a681a89950852e3d54f8f
Chris Tranter <135023953+Chris-Tranter@users.noreply.github.com>
2024-05-24 12:41:12 +0100

    added a fetch function! to retrieve temperature key and value! from the json API response

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,5 @@
+// THIS https://api.openweathermap.org/data/2.5/weather?q    =trowbridge    &appid=API KEY  &units=metric
+
 const apiKey = "a3f92f91eb9c3564f2a410ca17f082ba";
 const units = "metric";
 
@@ -7,10 +9,23 @@ let forecast = {
     cardiff: "17"
 };
 
-// create -search submit OUTPUT- elements
+// create -search + submit + output  elements
 const inputElement = document.getElementById("city-input");
 const buttonElement = document.getElementById("submit-button");
 const outputElement = document.getElementById("output-area");
+//calling the API from the openweathermap website URL
+const url = `https://api.openweathermap.org/data/2.5/weather?q=cardiff&appid=a3f92f91eb9c3564f2a410ca17f082ba&units=metric`;
+
+//a fetch function with a data response from apiURL website
+fetch(url)
+    .then(function (response) {
+        return response.json();
+    }) 
+    .then(function (data) {
+        console.log(data.main.temp);
+});
+
+
 
 // when -search- is clicked a function starts *if* the city matches the API's =correct answer *else* =error 
 buttonElement.addEventListener("click", function() {
@@ -25,5 +40,7 @@ buttonElement.addEventListener("click", function() {
     outputElement.innerText = output;
 });
 
+/*button click audio
 const audio = new Audio();
 audio.src = "../assets/sounds/soft.mp3";
+*/ 
